2b8d385c660efcd0c0b2df2e14ae9b09ed5fbc1b
Subhankar dutta <subhankar0810@gmail.com>
2021-12-07 02:06:37 +0530

    ayusha commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import urekonlogo from './images/urekon_logo.svg';
+import uemlogo from './images/UEM Logo (White).png';
+import urekontxt from './images/ureckon text logo.png';
+import form_back from './images/form field.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="back">
+      <div className="header">
+        <img src={uemlogo} alt="uem logo"/>
+        <img src={urekontxt} alt="uem logo"/>
+        <img src={urekonlogo} alt="urekon logo"/>
+      </div>
+      <div className="form-list">
+        <div className="field">
+          <label>
+            Email id
+          </label>
+          <div className="input-field">
+              <img src={form_back} className="back-image"/>
+              <input type="email" className="input-form-border"/>
+
+          </div>
+          
+        </div>
+        <div className="field">
+          <label>
+            Passcode
+          </label>
+          <div className="input-field">
+              <img src={form_back} className="back-image"/>
+              <input type="password" className="input-form-border"/>
+
+          </div>
+          
+        </div>
+        
+      </div>
     </div>
+    </>
   );
 }
 
